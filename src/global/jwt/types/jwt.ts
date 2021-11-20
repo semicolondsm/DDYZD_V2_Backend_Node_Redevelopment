80a6cd51b0d5ae11e90a4d47d56723a9f79bf236
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-21 06:34:40 +0900

    CREATE; jwt.ts jwt 타입

---
+++ src/global/jwt/types/jwt.ts
@@ -0,0 +1,9 @@
+export type JwtPayload = {
+    iss?: string | undefined;
+    sub?: string | undefined;
+    aud?: string | string[] | undefined;
+    exp?: number | undefined;
+    nbf?: number | undefined;
+    iat?: number | undefined;
+    jti?: string | undefined;
+};
