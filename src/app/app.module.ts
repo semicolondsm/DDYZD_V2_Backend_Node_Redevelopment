ac9e6d4647e0bb1b42911f112774dc620cf43ee1
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-21 19:51:17 +0900

    UPDATE; app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from 'src/app/app.controller';
 import { AppService } from 'src/app/app.service';
 import { TypeormConfigModule } from 'src/global/typeorm-config/typeorm-config.module';
 import { ClubModule } from 'src/domain/club/club.module';
+import { UserModule } from "src/domain/user/user.module";
 
 @Module({
-    imports: [TypeormConfigModule, ClubModule],
+    imports: [TypeormConfigModule, ClubModule, UserModule],
     controllers: [AppController],
     providers: [AppService],
 })
