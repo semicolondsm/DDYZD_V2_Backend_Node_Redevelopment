334c98c0071ee06883466078c34b88a6aa2bde34
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-21 20:06:44 +0900

    UPDATE; jwt guard factory

--- src/global/jwt/jwt-guard-factory.ts
+++ src/global/jwt/jwt-guard-factory.ts
@@ -1,21 +1,25 @@
 import { BasicVerifyTokenGuard } from './basic-verify-token.guard';
 import { OptionalVerifyTokenGuard } from './optional-verify-token.guard';
 import { VerifyTokenGuard } from './verify-token.guard';
+import { VerifyRefreshTokenGuard } from "./verify-refresh-token.guard";
 
 export class JwtGuardFactory {
     private static basicVerifyTokenGuard: VerifyTokenGuard =
         new BasicVerifyTokenGuard();
     private static optionalVerifyTokenGuard: VerifyTokenGuard =
         new OptionalVerifyTokenGuard();
+    private static  verifyRefreshTokenGuard: VerifyRefreshTokenGuard = new VerifyRefreshTokenGuard();
 
     public static createJwtGuard(
-        type: 'REQUIRED' | 'OPTIONAL',
+        type: 'REQUIRED' | 'OPTIONAL' | 'REFRESH',
     ): VerifyTokenGuard {
         switch (type) {
             case 'REQUIRED':
                 return this.basicVerifyTokenGuard;
             case 'OPTIONAL':
                 return this.optionalVerifyTokenGuard;
+            case 'REFRESH':
+                return this.verifyRefreshTokenGuard;
         }
     }
 }
