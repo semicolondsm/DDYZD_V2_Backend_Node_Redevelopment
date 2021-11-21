a4881ae2356143e6e1b15919d21edc8d8582f066
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-21 16:03:47 +0900

    UPDATE; club moule

--- src/domain/club/club.module.ts
+++ src/domain/club/club.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ClubService } from './club.service';
 import { ClubController } from './club.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClubRepository } from 'src/domain/club/domain/club.repository';
+import { ClubTagViewRepository } from 'src/domain/club/domain/hastag/club-tag.view.repository';
+import { UserRepository } from 'src/domain/user/domain/user.repository';
+import { ClubUserViewRepository } from 'src/domain/club/domain/user/club-user.view.repository';
+import { ClubHeadRepository } from 'src/domain/club/domain/head/club-head.repository';
+import { ClubFollowRepository } from 'src/domain/club/domain/follow/club-follow.repository';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([
+            ClubRepository,
+            ClubTagViewRepository,
+            UserRepository,
+            ClubUserViewRepository,
+            ClubHeadRepository,
+            ClubFollowRepository,
+        ]),
+    ],
     providers: [ClubService],
     controllers: [ClubController],
 })
