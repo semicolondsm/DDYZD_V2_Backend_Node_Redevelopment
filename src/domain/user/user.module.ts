df1e5e443e7cecd331aee29a44f8c841116bbc08
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-21 19:51:03 +0900

    UPDATE; user module

--- src/domain/user/user.module.ts
+++ src/domain/user/user.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { GithubModule } from 'src/infrastructure/github/github.module';
+import { DsmAuthModule } from 'src/infrastructure/dsmauth/dsmauth.module';
+import { EnvironmentConfigModule } from 'src/global/environment-config/environment-config.module';
+import { EnvironmentConfigService } from 'src/global/environment-config/environment-config.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserRepository } from "src/domain/user/domain/user.repository";
+import { ClubUserViewRepository } from "src/domain/club/domain/user/club-user.view.repository";
 
 @Module({
+    imports: [
+        GithubModule.register(),
+        DsmAuthModule.register(),
+        EnvironmentConfigModule,
+        TypeOrmModule.forFeature([UserRepository, ClubUserViewRepository])
+    ],
     controllers: [UserController],
-    providers: [UserService],
+    providers: [UserService, EnvironmentConfigService],
 })
 export class UserModule {}
