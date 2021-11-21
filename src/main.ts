59a11b35c05b8bb9e491c3880b5941a129f6e06c
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-21 21:21:15 +0900

    FIX; all origin enable

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from 'src/app/app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
+    app.enableCors({ origin: "*", credentials: true });
     await app.listen(3000);
 }
 bootstrap();
